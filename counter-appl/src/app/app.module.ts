6e4e79dc550bf04cc7a1b07e172abacda245a453
Pavel Bernshtam <pavelb@cgen.com>
2018-05-11 15:10:31 +0300

    fix angular app module. rebuild

--- counter-appl/src/app/app.module.ts
+++ counter-appl/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HttpModule } from '@angular/http'
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+	HttpClientModule,
+	HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
